f3ca073c5823ecc6075727142a82b0295b0badb6
Cristian Yulma <cristianyulmaw@gmail.com>
2025-07-14 09:22:24 +0700

    Fix: Fixing code using eslint

--- src/no2.js
+++ src/no2.js
@@ -1,17 +1,17 @@
 const getDataFromServer = (status, callback)=>{
     if(status){
-    setTimeout(()=>{
-    const products = [
-        'Product 1',
-        'Product 2',
-        'Product 3'
-    ]
-callback(products, null)
-},3000)
-}else{
-const err = new Error('Failed to fetch data')
-callback(null, err)
-}
-}
+        setTimeout(()=>{
+            const products = [
+                "Product 1",
+                "Product 2",
+                "Product 3"
+            ];
+            callback(products, null);
+        },3000);
+    }else{
+        const err = new Error("Failed to fetch data");
+        callback(null, err);
+    }
+};
 
-export default getDataFromServer
+export default getDataFromServer;
