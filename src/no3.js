b365843e05bae97cf2b4f11e8f864b28ff16f455
Cristian Yulma <cristianyulmaw@gmail.com>
2025-07-14 08:57:58 +0700

    Feat: Making usersort function features

---
+++ src/no3.js
@@ -0,0 +1,18 @@
+async function usersort() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const user = await response.json()
+
+        const usercity = user.map(user => ({
+            name: user.name,
+            city: user.address.city
+        }))
+
+        usercity.sort((a,b) => a.city.localeCompare(b.city))
+        console.log(usercity)
+    } catch (error) {
+        console.log('Gagal dalam pengambilan data',error.message)
+    }
+}
+
+export default usersort
