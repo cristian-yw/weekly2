eb2a96644a6dbc22354d108617c0761c3445197c
Cristian Yulma <cristianyulmaw@gmail.com>
2025-07-14 08:58:26 +0700

    Feat: Making divideAndSort function features

---
+++ src/no4.js
@@ -0,0 +1,16 @@
+function divideAndSort(number){
+    const divide = number.toString().split('0')
+
+    const sortpart = divide.map(divide => {
+        return divide
+        .split('')
+        .sort((a,b)=> a - b)
+        .join('')
+
+    })
+    const result = sortpart.join('')
+    
+    return Number(result)
+}
+
+export default divideAndSort
