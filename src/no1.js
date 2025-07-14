ed9bb5036cd1d748a5f72fee32cfbc6c96bea4f0
Cristian Yulma <cristianyulmaw@gmail.com>
2025-07-14 14:37:11 +0700

    Fix: Adding comment arcording to the task 1

--- src/no1.js
+++ src/no1.js
@@ -9,5 +9,22 @@ const fetchData = (status) => {
         }
     });
 };
+// // 1. Promise:
+//    - Promise adalah objek di JavaScript yang merepresentasikan operasi asynchronous.
+//    - Promise memiliki 3 keadaan: pending, fulfilled (berhasil), dan rejected (gagal).
+//    
+
+// 2. then-catch:
+//    - then digunakan untuk menangani kondisi ketika Promise berhasil (fulfilled).
+//    - catch digunakan untuk menangani kondisi ketika Promise gagal (rejected).
+
+// 3. async/await:
+//    - async adalah keyword untuk mendefinisikan fungsi asynchronous.
+//    - await digunakan untuk menunggu hasil Promise tanpa perlu menggunakan then.
+//    - await harus digunakan di dalam fungsi yang dideklarasikan dengan async.
+
+// 4. try-catch:
+//    - try digunakan untuk mengeksekusi blok kode yang mungkin menyebabkan error.
+//    - catch digunakan untuk menangkap error, termasuk error dari Promise yang gagal saat digunakan dengan await.
 
 export default fetchData;
